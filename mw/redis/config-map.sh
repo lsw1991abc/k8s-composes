43d4c9280ea520a9fdd5e3f9d4e69ff6fc3869e4
Wizard <lsw1991abc@gmail.com>
2021-06-02 16:09:45 +0800

    mysql

--- mw/redis/config-map.sh
+++ mw/redis/config-map.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kubectl create configmap redis-conf-map -n mw --from-file /data/k8s-volume//redis//conf/redis.conf -o yaml --dry-run=client | kubectl replace -f -
+kubectl create configmap redis-conf-map -n mw --from-file /data/k8s-volume/redis/conf/redis.conf -o yaml --dry-run=client | kubectl replace -f -
